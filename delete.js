b64522f6017627e50b3ca7d7232cf12d15041dac
nikumaruku <74139509+nikumaruku@users.noreply.github.com>
2023-06-04 12:48:12 +0800

    Update delete.js
    Added stringQueryParameters to obtain uuid from API. Allow user to delete specific book instead of whole database.

--- delete.js
+++ delete.js
@@ -1,26 +1,44 @@
-import { DynamoDBClient} from "@aws-sdk/client-dynamodb";
-import {DynamoDBDocumentClient, DeleteCommand} from "@aws-sdk/lib-dynamodb";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, DeleteCommand } from "@aws-sdk/lib-dynamodb";
 
 const client = new DynamoDBClient({});
 const ddb = DynamoDBDocumentClient.from(client);
 
 const tableName = "booktrack-book";
 
-export const handler = async(event) => {
+export const handler = async (event) => {
+    console.log(event);
     
-    const DCI = {
-            TableName : tableName,
-            Key : {
-                  uuid: event.uuid,
+    const id = event.queryStringParameters.uuid;
+    
+    try {
+        const DCI = {
+            TableName: tableName,
+            Key: {
+                uuid: id,
             }
-    };
-    
-    const result = await ddb.send(new DeleteCommand(DCI));
-    console.log(result);
-        
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify(result),
-    };
-    return response;
+        };
+
+        const result = await ddb.send(new DeleteCommand(DCI));
+        console.log(result);
+
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify({message:"Item deleted successfully"}),
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Credentials": true,
+            }
+        };
+        return response;
+
+    }
+    catch (e) {
+        console.log(e);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: "Error deleting item" }),
+        };
+    }
+
 };
